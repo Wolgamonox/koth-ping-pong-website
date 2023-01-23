98ee3b29d8f6d971c295557f28866eaac745b849
Gaston Wolfart <gastonwolfart@gmail.com>
2023-01-23 20:31:43 +0100

    continued server, need to implement serving of graphs

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+from summary_creator import *
+
+from flask import Flask, request, render_template, redirect, url_for
 from flask_qrcode import QRcode
 
 from datetime import datetime
@@ -8,47 +8,56 @@ import netifaces
 import json
 import pandas as pd
 
-import os  
-os.makedirs('games_data', exist_ok=True) 
+import webbrowser
+import io
 
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+import os
+
+os.makedirs('games_data', exist_ok=True)
 
 app = Flask(__name__)
 qrcode = QRcode(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
 @app.route("/show-qr")
 def show_qr():
-    
-    # To get host ip
-    for iface in netifaces.interfaces():
-        iface_details = netifaces.ifaddresses(iface)
-        if netifaces.AF_INET in iface_details:
-            for ip_interfaces in iface_details[netifaces.AF_INET]:
-                for key, ip_add in ip_interfaces.items():
-                    if key == 'addr' and ip_add != '127.0.0.1' and not ip_add.startswith('172'):
-                        host_ip = ip_add
-
-
-    host_name = '%s:%d' % (host_ip, 17809)
+    host_name = get_hostname()
     return render_template('qr_code.html', host_name=host_name)
+
+
+@app.route('/summary')
+def summary():
+    return render_template('game_summary.html', filename=request.args['filename'])
+
+
+@app.route('/plot_summary.png')
+def plot_summary():
+    summary_filename = request.args.get('filename')
+    fig = generate_summary(summary_filename)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    parse_json_to_csv(request.data.decode())
-
-    return "Success", 200
-
-
-def parse_json_to_csv(raw_json):
-    now = datetime.now().strftime("%d.%m.%Y %H_%M_%S")
+    now = datetime.now().strftime("%d-%m-%Y %H_%M_%S")
     filename = 'games_data/game (%s).csv' % now
 
     print(filename)
-    
+    parse_json_to_csv(filename, request.data.decode())
+
+    save_summary(filename)
+
+    webbrowser.open('http://' + get_hostname() + url_for('plot_summary', filename=filename))
+
+    return 'Success', 200
+
+
+def parse_json_to_csv(filename, raw_json):
     transitions = json.loads(raw_json)['transitions']
     df = pd.DataFrame({
         'Name': [list(transition.keys())[0] for transition in transitions],
@@ -57,3 +66,17 @@ def parse_json_to_csv(raw_json):
 
     print(df)
     df.to_csv(filename)
+
+
+def get_hostname():
+    # To get host ip
+    for iface in netifaces.interfaces():
+        iface_details = netifaces.ifaddresses(iface)
+        if netifaces.AF_INET in iface_details:
+            for ip_interfaces in iface_details[netifaces.AF_INET]:
+                for key, ip_add in ip_interfaces.items():
+                    # filter localhost and WSL interface
+                    if key == 'addr' and ip_add != '127.0.0.1' and not ip_add.startswith('172'):
+                        host_ip = ip_add
+
+    return '%s:%d' % (host_ip, 17809)
---
+++ summary_creator.py
@@ -0,0 +1,72 @@
+from datetime import timedelta
+
+import pandas as pd
+import numpy as np
+
+from matplotlib.figure import Figure
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+
+def generate_summary(filename):
+    """Function to plot summary of game from filename."""
+
+    df = pd.read_csv(filename).drop('Unnamed: 0', axis=1)
+    total_duration_seconds = df['Interval'].sum()
+
+    player_names = df['Name'].unique()
+    player_colors = {name:color for name, color in zip(player_names, sns.color_palette('Pastel1')[:len(player_names)])}
+
+    crowns_claimed = df.drop(index=0).groupby('Name').count().rename({'Interval': 'Claimed'}, axis=1).sort_values('Claimed', ascending=False)
+    total_time_king = df.groupby('Name').sum().rename({'Interval': 'Duration'}, axis=1).sort_values('Duration', ascending=False)
+    mean_reign_time = df.groupby('Name').mean().rename({'Interval': 'Duration'}, axis=1).sort_values('Duration', ascending=False)
+
+
+    df['interval_perc'] = df['Interval'].apply(lambda x: int(np.ceil(x/total_duration_seconds * 100)))
+
+    graph_vector = []
+
+    for _, transition in df.iterrows():
+        graph_vector.extend(transition.Name for i in range(transition.interval_perc))
+
+    print('Game duration: %s ' % str(timedelta(seconds=int(total_duration_seconds))))
+    print('First king: %s | Last king: %s ' % (df.iloc[0]['Name'], df.iloc[-1]['Name']))
+
+    fig = Figure(tight_layout=True, figsize=(10, 6))
+    gs = gridspec.GridSpec(2, 3)
+
+    # Total Time as king
+    ax = fig.add_subplot(gs[0, 0])
+    ax.pie(total_time_king['Duration'], labels=total_time_king.index, autopct='%.0f%%', colors=[player_colors[key] for key in player_colors])
+    ax.set_title('Fraction of time as king')
+
+    # Mean Reign time
+    ax = fig.add_subplot(gs[0, 1])
+    sns.barplot(x=mean_reign_time.index, y=mean_reign_time.Duration, palette=player_colors, ax=ax)
+    ax.set_xlabel('')
+    ax.set_ylabel('Seconds')
+    ax.set_title('Mean reign time')
+
+    # Crowns claimed
+    ax = fig.add_subplot(gs[0, 2])
+    sns.barplot(x=crowns_claimed.index, y=crowns_claimed.Claimed, palette=player_colors, ax=ax)
+    ax.set_yticks(range(0, int(np.ceil(crowns_claimed['Claimed'].max())) + 1))
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.set_title('Number of crowns claimed')
+
+    # Transition graph
+    ax = fig.add_subplot(gs[1, :])
+    sns.lineplot(x=range(len(graph_vector)), y=graph_vector, drawstyle='steps', ax=ax)
+    ax.set_xlabel('Game advancement (percent)')
+    ax.set_title('Visualization of crown transitions')
+
+    fig.suptitle('Game Summary')
+
+    return fig
+
+def save_summary(filename:str):
+    fig = generate_summary(filename)
+
+    path = filename.split('.')[0] + '.png'
+
+    fig.savefig(path)
