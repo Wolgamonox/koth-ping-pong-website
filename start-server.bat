cf688296121b923523c172851e7c6a707372a8c4
Gaston Wolfart <gastonwolfart@gmail.com>
2023-01-26 19:08:43 +0100

    changed start script for faster launch

--- start-server.bat
+++ start-server.bat
@@ -1,4 +1,9 @@
+:: Python requirements installation
 call venv\Scripts\activate.bat
 pip install -r requirements.txt -q
-explorer http://127.0.0.1:17809/show-qr
-flask --app server run --host=0.0.0.0 -p 17809
+
+:: Start server
+start "Server" flask --app server run --host=0.0.0.0 -p 17809
+
+:: Open browser to show QR code
+start "" explorer http://127.0.0.1:17809/show-qr
