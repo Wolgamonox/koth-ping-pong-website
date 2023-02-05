440f35d99b96d041bfcf306e5a985b0599e741aa
Gaston Wolfart <35776858+Wolgamonox@users.noreply.github.com>
2023-02-05 02:00:40 +0100

    modified frontend a bit

--- django_ping_pong_koth/games/urls.py
+++ django_ping_pong_koth/games/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "games"
 urlpatterns = [
-    path("", views.AllGamesView.as_view(), name="all-games"),
-    path("<int:pk>/", views.GameDetailView.as_view(), name="game-detail"),
+    path("", views.AllGamesView.as_view(), name="all"),
+    path("<int:pk>/", views.GameDetailView.as_view(), name="detail"),
+    # API for mobile app
     path("upload", views.upload_game),
     path("get_pk_of", views.get_pk_of),
 ]
