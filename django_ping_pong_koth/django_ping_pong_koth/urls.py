0b46f4f5ad72a8b04e7b5e22dfbd3a122600eefb
Gaston Wolfart <gastonwolfart@gmail.com>
2023-04-07 19:01:28 +0200

    started accounts app, maybe broken

--- django_ping_pong_koth/django_ping_pong_koth/urls.py
+++ django_ping_pong_koth/django_ping_pong_koth/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("home.urls")),
     path("games/", include("games.urls")),
+    path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
