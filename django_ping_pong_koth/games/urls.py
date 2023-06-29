537254c52c8518d0144ce50cd44154cea5307af4
Gaston Wolfart <gastonwolfart@gmail.com>
2023-06-29 11:39:22 +0200

    added games viewset and pagination

--- django_ping_pong_koth/games/urls.py
+++ django_ping_pong_koth/games/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
+
+router = DefaultRouter()
+router.register("all", views.GamesViewSet)
 
 app_name = "games"
 urlpatterns = [
     path("", views.AllGamesView.as_view(), name="all"),
     path("<int:pk>/", views.GameDetailView.as_view(), name="detail"),
     path("upload", views.upload_game),  # API for mobile app
-]
+] + router.urls
