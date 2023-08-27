136ede63d8a14556b448e71fffde8a5c5ce3c45d
Gaston Wolfart <gastonwolfart@gmail.com>
2023-08-28 00:17:20 +0200

    updated requirements and added pytest conf

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -9,6 +9,7 @@ router.register("all", views.GamesViewSet)
 app_name = "games"
 urlpatterns = [
     path("", views.AllGamesView.as_view(), name="all"),
+    # TODO need to remove old api urls
     path("<int:pk>/", views.GameDetailView.as_view(), name="detail"),
     path("upload", views.upload_game),  # API for mobile app
 ] + router.urls
