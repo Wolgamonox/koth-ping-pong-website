841be1c72209e789c31d72a3da4ed3dde994e862
Gaston Wolfart <gastonwolfart@gmail.com>
2023-08-27 15:47:36 +0200

    added str repr for adminpage

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 # Create your models here.
 class CustomUser(AbstractUser):
     class Meta:
         db_table = "auth_user"
+
+    def __str__(self) -> str:
+        return f"{self.username} (pk:{self.pk})"
